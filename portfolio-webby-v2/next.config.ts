1b5bbfd9d8b4f760b23abc3d1fdf0a8125e7f98a
joseph101kt <joseph101kt@gmail.com>
2025-08-21 10:57:42 +0530

    final commit

--- portfolio-webby-v2/next.config.ts
+++ portfolio-webby-v2/next.config.ts
@@ -1,12 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: [
-      'picsum.photos',
-    ],
-  }
-};
+    domains: ['hrghmxifjkbkhurxmzhy.supabase.co'],
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig

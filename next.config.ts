01eaff6f1e8fe5b272cf69630c345d25cbaa38bf
joseph101kt <joseph101kt@gmail.com>
2025-09-09 18:16:04 +0530

    made some change in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['hrghmxifjkbkhurxmzhy.supabase.co'],
+  },
+  webpack: (config: { resolve: { alias: { [x: string]: unknown; }; }; }) => {
+    if (config.resolve && config.resolve.alias) {
+      config.resolve.alias['@'] = path.resolve(__dirname);
+    }
+    return config;
   },
 }
 
